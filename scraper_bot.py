ea0d055ad4a4eb09abcd8d0c2869b77f13771bbc
alereyesx <63877846+alereyesx@users.noreply.github.com>
2020-08-19 12:49:11 -0400

    added log chat

--- scraper_bot.py
+++ scraper_bot.py
@@ -1,39 +1,46 @@
 from telethon import TelegramClient, events, sync
-import env,time
+import env, time
 
 api_id = env.API_ID
 api_hash = env.API_HASH
 chat = env.CHAT
-SLEEP_MINUTES = 60*3
+log_chat = env.LOG_CHAT
+SLEEP_MINUTES = 60 * 3
 MIN_MSG_ID = float('-inf')
-KEYWORDS = ['#monaco','#boyeroycamagüey','#boyerosycamaguey','#boyerosycamagüey','#camagüeyyboyeros','#camagüeyyboyero','#boyero','#camagüey','#boyeros','#ventoysantacatalina','#split','#splits']
+KEYWORDS = ['#monaco', '#boyeroycamagüey', '#boyerosycamaguey', '#boyerosycamagüey', '#camagüeyyboyeros',
+            '#camagüeyyboyero', '#boyero', '#camagüey', '#boyeros', '#ventoysantacatalina', '#split', '#splits',
+            '#freezer']
 
 # telethon_client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
 client = TelegramClient('session1', api_id, api_hash)
-client.start(phone=env.PHONE,password=env.PASSWORD)
+client.start(phone=env.PHONE, password=env.PASSWORD)
 
 
 def detect_relevantinfo(messages):
-
     print("Checking messages\n")
     for message in messages:
         for keyword in KEYWORDS:
             message_lower = message.message.lower()
             if keyword in message_lower:
                 message.forward_to(chat)
-                
+
 
 while 1:
-    print('start')
+    # print('start')
+    client.send_message(entity=log_chat,
+                        message=f'start')
 
-    messages = client.get_messages('DondeHayEnLaHabana',limit=50,min_id=MIN_MSG_ID)
+    messages = client.get_messages('DondeHayEnLaHabana', limit=50, min_id=MIN_MSG_ID)
 
     if len(messages) > 0:
         MIN_MSG_ID = messages[0].id
-        print(f'Min message id: {MIN_MSG_ID}, {messages[0].message}\n')
+        client.send_message(entity=log_chat,
+                            message=f'Min message id: {MIN_MSG_ID}, {messages[0].message}\n')
+        # print(f'Min message id: {MIN_MSG_ID}, {messages[0].message}\n')
         detect_relevantinfo(messages)
 
-    print(f'Sleeping for {SLEEP_MINUTES} minutes\n')
+    # print(f'Sleeping for {SLEEP_MINUTES} minutes\n')
+    client.send_message(entity=log_chat,
+                        message=f'Sleeping for {SLEEP_MINUTES} minutes\n')
     time.sleep(SLEEP_MINUTES)
-
